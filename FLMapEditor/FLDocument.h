3a8b55af0b2871ee1868faabf0fb9d056545aaa3
Chang-Gi, Kim <cgkim@mac.com>
2013-04-08 16:33:30 +0900

    Initial Commit

---
+++ FLMapEditor/FLDocument.h
@@ -0,0 +1,13 @@
+//
+//  FLDocument.h
+//  FLMapEditor
+//
+//  Created by cgkim on 13. 4. 8..
+//  Copyright (c) 2013년 cgkim. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface FLDocument : NSDocument
+
+@end
